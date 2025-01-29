7c2b756350852f031d925e364062d3b3c357986b
Sandeep Nilaji <snilaji@gmail.com>
2025-01-29 18:13:46 +0530

    Update CandidateDetail.jsx

--- pages/components/CandidateDetail.jsx
+++ pages/components/CandidateDetail.jsx
@@ -1,10 +1,7 @@
 export default function CandidateDetail({ candidate }) {
   return (
     <div className="p-4">
-      <h2 className="text-lg font-bold">{candidate.name}</h2>
-      <p>Experience: {candidate.experience} years</p>
-      <p>Can join in: {candidate.canJoinIn} days</p>
-      <p>Minimum salary expected: ${candidate.salary}k</p>
+    
     </div>
   );
 }
